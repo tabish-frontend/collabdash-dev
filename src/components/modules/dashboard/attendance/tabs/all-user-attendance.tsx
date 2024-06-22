65772f216ddb3b3406e7e5e010d939243a3dc489
Tabish <thdevthree@gmail.com>
2024-06-22 16:02:08 +0500

    1 change

--- src/components/modules/dashboard/attendance/tabs/all-user-attendance.tsx
+++ src/components/modules/dashboard/attendance/tabs/all-user-attendance.tsx
@@ -201,7 +201,7 @@ export const AllUserAttendance: React.FC<AllUserAttendanceProps> = ({
         }}
         spacing={1}
         gap={1}
-        my={4}
+        my={2}
         justifyContent={"space-between"}
       >
         <Stack
